a0912f3853cf55e00b3c0b47d3ebfc476bfd0e36
PriyanshiKhorwal <priyanshikhorwal0905@gmail.com>
2026-04-27 20:19:28 +0530

    structure formation

---
+++ backend/src/main/java/com/project/IndoorNavigationApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndoorNavigationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndoorNavigationApplication.class, args);
+	}
+
+}
